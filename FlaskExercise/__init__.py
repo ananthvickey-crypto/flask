504662a7c3cff8df8b875a1077ab9a35be7e1610
Ananth <mananth3112@gmail.com>
2025-10-08 13:09:39 +0530

    Initial commit - Flask app

---
+++ FlaskExercise/__init__.py
@@ -0,0 +1,12 @@
+import logging
+from flask import Flask
+
+app = Flask(__name__)
+wsgi_app = app.wsgi_app
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
+# TODO: Set the app's logger level to "warning"
+#   and any other necessary changes
+
+import FlaskExercise.views
